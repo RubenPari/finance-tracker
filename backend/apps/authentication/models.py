d2c5e6505c1d40207c3532376189517ccb6314a0
RubenPari <rubenpari.developer@gmail.com>
2026-05-06 02:20:42 +0200

    Remove redundant username from REQUIRED_FIELDS since email is the auth identifier

--- backend/apps/authentication/models.py
+++ backend/apps/authentication/models.py
@@ -6,7 +6,7 @@ class User(AbstractUser):
     email = models.EmailField(unique=True)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
+    REQUIRED_FIELDS = []
 
     class Meta:
         db_table = 'users'
